af0eb3f615656c03ac8d83625691b1334bade3bb
SimonHEIG <simon.meia@heig-vd.ch>
2022-05-30 09:53:28 +0200

    fix registercontroller

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\RegisterRequest;
+use App\Models\Group;
 use App\Rules\GroupPwd;
 
 class RegisterController extends Controller
