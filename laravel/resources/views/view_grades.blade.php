5d50a7e597b9e536bad75add67b8bfd48c5e1212
SimonHEIG <simon.meia@heig-vd.ch>
2022-05-31 09:10:39 +0200

    Ajout des branches sans notes

--- laravel/resources/views/view_grades.blade.php
+++ laravel/resources/views/view_grades.blade.php
@@ -5,6 +5,37 @@
 
 @section('contenu')
 
-    {{$grades}}
+@foreach($gradesArray as $moduleName => $moduleData)
+
+    <h1>{{$moduleName}}</h1>
+
+    @foreach($moduleData as $courseName => $courseData)
+
+        @if($courseName != 'average')
+            <h3>{{$courseName}}</h3>
+
+            @foreach($courseData['grades'] as $grade => $gradeData)
+                Note : {{$gradeData['grade']}}
+                 - 
+                Coefficient : {{$gradeData['coefficient']}}
+                <br>
+            @endforeach
+
+            Weighting : {{$courseData['weighting']}}
+            <br>
+            Moyenne de branche : {{$courseData['average']}}
+
+        <br>
+    @endif
+
+@endforeach
+
+---<br>
+Moyenne de module : {{$moduleData['average']}}
+
+
+@endforeach
+
+
 
 @endsection
