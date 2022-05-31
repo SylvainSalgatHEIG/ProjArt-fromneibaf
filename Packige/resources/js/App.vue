af6eae550e0288206cb485a3097beb3aa6e14709
Emile <emile.vianneyliaud@gmail.com>
2022-05-31 10:57:43 +0200

    change laravel installation

---
+++ Packige/resources/js/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import {ref} from 'vue';
+
+const msg = ref('');
+</script>
+
+<template> 
+	Hello vue 3
+</template>
---
+++ Packige/resources/views/vue.blade.php
@@ -0,0 +1,15 @@
+{{-- resources/views/vue.blade.php --}}
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel 9 Vue 3 - DevDuniya.com</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+	<div id="app"></div>
+
+	<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
