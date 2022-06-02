4c89d28971283c94e12c3d1c2d4a21bfca250cce
SimonHEIG <simon.meia@heig-vd.ch>
2022-06-02 13:12:23 +0200

    Affichage notes ok

--- Packige/resources/js/views/grades.vue
+++ Packige/resources/js/views/grades.vue
@@ -10,12 +10,17 @@ const { data: gradesArray } = useFetch("/api/grades/");
 <template>
   <div v-for="(moduleData, moduleName) in gradesArray">
     <h1>{{ moduleName }}</h1>
-    <div v-for="(coursesData, coursesName) in moduleData">
-		<h2 v-if="coursesName != 'average'">{{coursesName}}</h2>
-		{{coursesData}}
-		<div v-for="(courseData, courseName) in coursesData">
-		{{courseName}}
+	Moyenne de module : {{moduleData.average}}
+    <div v-for="(courseData, courseName) in moduleData">
+      <div v-if="courseName != 'average'">
+        <h2>{{ courseName }}</h2>
+        Pondération : {{ courseData.weighting }}
+		<br>
+        <div v-for="(gradeData) in courseData.grades">
+			Note : {{gradeData.grade}} | Pondération : {{gradeData.coefficient}}
 		</div>
-	</div>
+		Moyenne : {{courseData.average}}
+      </div>
+    </div>
   </div>
 </template>
