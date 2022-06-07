7919476331f317f2b56d359c2deb9565abe597f9
Paul Mairot <paul.mairot@heig-vd.ch>
2022-06-07 08:44:15 +0200

    Start implementing links page

--- Packige/resources/js/views/links.vue
+++ Packige/resources/js/views/links.vue
@@ -3,8 +3,58 @@ import {ref} from 'vue';
 
 const msg = ref('');
 
+let currentCategory = "";
+
+const links = [
+	{
+		"name":"Attestation d'études",
+		"category":"Semestre",
+		"link":"https://www.heig-vd.ch"
+	},
+	{
+		"name":"Calendrier Académique",
+		"category":"Semestre",
+		"link":"https://www.heig-vd.ch"
+	},
+	{
+		"name":"Justificatif d'absences",
+		"category":"Absences",
+		"link":"https://www.heig-vd.ch"
+	},
+	{
+		"name":"Webmail",
+		"category":"Absences",
+		"link":"https://www.heig-vd.ch"
+	},
+	{
+		"name":"Bulletin",
+		"category":"Notes",
+		"link":"https://www.heig-vd.ch"
+	},
+	{
+		"name":"Contrôles continus",
+		"category":"Notes",
+		"link":"https://www.heig-vd.ch"
+	},
+	{
+		"name":"Enseignement à choix",
+		"category":"Enseignement",
+		"link":"https://www.heig-vd.ch"
+	},
+	{
+		"name":"Eval. des enseignants",
+		"category":"Enseignement",
+		"link":"https://www.heig-vd.ch"
+	}
+
+]
+
 </script>
 
 <template>
 	<h1>Page Links</h1>
+	<div v-for="(link) in links">
+		<h2 v-if="link.category != currentCategory">{{currentCategory = link.category}}</h2>
+		<a :href=link.link>{{link.name}}</a>
+	</div>
 </template>
