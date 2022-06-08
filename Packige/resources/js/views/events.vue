c12ab3deef0e2a9774c87367ce1fe0a46f1f8b6d
AlexSoutoHEIG <alexandr.souto@heig-vd.ch>
2022-06-08 16:56:26 +0200

    Events CSS
    Fin de journée 08.06
    Premier jet de CSS de la page event
    (Manque image crop et h2)

--- Packige/resources/js/views/events.vue
+++ Packige/resources/js/views/events.vue
@@ -1,17 +1,30 @@
 <script setup>
-import {ref, computed } from 'vue';
-import { useFetch } from '../composables/fetch';
-import { useLocalstorage } from '../composables/localstorage';
-import { apiEvents} from '../config/apiUrls.js';
+import { ref, computed } from "vue";
+import { useFetch } from "../composables/fetch";
+import { useLocalstorage } from "../composables/localstorage";
+import { apiEvents } from "../config/apiUrls.js";
 
-const {data: events} = useFetch(apiEvents);
+const { data: events } = useFetch(apiEvents);
 const year = ref(new Date().getFullYear());
 
-const {value: theEvents} = useLocalstorage('events', events.value);
-const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+const { value: theEvents } = useLocalstorage("events", events.value);
+const months = [
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "août",
+  "septembre",
+  "octobre",
+  "novembre",
+  "décembre",
+];
 
 function getFormattedDate(day, month, year) {
-	return day + "." + formatTwoDigits(months.indexOf(month)) + "." + year;
+  return day + "." + formatTwoDigits(months.indexOf(month)) + "." + year;
 }
 
 /**
@@ -21,63 +34,111 @@ function getFormattedDate(day, month, year) {
  * @return {date}
  */
 function formatTwoDigits(date) {
-  if (date < 10)
-    return `0${date}`;
-  else
-    return date;
+  if (date < 10) return `0${date}`;
+  else return date;
 }
 
-
 const allEvents = computed(() => {
-	if (!theEvents.value) {
-		theEvents.value = events.value;
-	}
-	theEvents.value.forEach((value, index, array) => {
-		value.date = getFormattedDate(value.day, value.month, new Date().getFullYear());
-	})
+  if (!theEvents.value) {
+    theEvents.value = events.value;
+    return [];
+  }
+  theEvents.value.forEach((value, index, array) => {
+    value.date = getFormattedDate(
+      value.day,
+      value.month,
+      new Date().getFullYear()
+    );
+  });
   return theEvents.value;
 });
 </script>
 
 <template>
-	<div id="events-cards">
-		<div v-for="event in allEvents" class="event-card">
-			<img :src="event.imageLink" :alt="event.name">
-			<h1 class="title">{{event.name}}</h1>
-			<div class="date">
-				<span>{{event.date}}</span>
-			</div>
-			<a :href="event.link">{{event.name}}</a>
-		</div>
-	</div>
+  <h1>Actus</h1>
+  <div id="events-cards">
+    <div v-for="event in allEvents" class="event-card">
+      <img class="img" :src="event.imageLink" :alt="event.name" />
+      <div class="date">
+        <span>{{ event.date }}</span>
+      </div>
+      <h2 class="title">{{ event.name }}</h2>
+
+      <a class="link" :href="event.link">Lien vers l'événement</a>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-	#events-cards {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-around;
-	}
+body {
+  margin: 30px;
+}
+#events-cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
 
-	#events-cards .event-card {
-		background: linear-gradient(177.03deg, rgba(30, 17, 41, 0.6) -119.33%, rgba(115, 195, 207, 0.6) 379.63%);
-		box-shadow: -1px 1px 8px 4px rgba(0, 0, 0, 0.25);
-		border-radius: 15px;
-		/* width: 30%;
+#events-cards .event-card {
+  background: linear-gradient(
+    177.03deg,
+    rgba(30, 17, 41, 0.6) -119.33%,
+    rgba(115, 195, 207, 0.6) 379.63%
+  );
+  box-shadow: -1px 1px 8px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+  margin-bottom: 15px;
+  width: 330px;
+  height: 240px;
+
+  /* width: 30%;
 		margin: 1%;
 		padding: 1%;
 		background-color: #fff;
 		border-radius: 5px;
 		box-shadow: 0px 0px 5px #000; */
-	}
+}
 
-	#events-cards .event-card .date {
-		padding: 10px;
-		position: relative;
-		top: 0;
-		left: 10px;
+#events-cards .event-card .date {
+  padding: 3px;
+  position: absolute;
 
-		background: #F84E35;
-		border-radius: 15px;
-	}
+  width: 90px;
+  height: 30px;
+    left: 50px;
+	margin-top:25px;
+
+  font-family: "Inter";
+  font-weight: bold;
+  text-align: center;
+
+  background: #f84e35;
+  border-radius: 15px;
+}
+.img {
+  border-radius: 15px;
+  width: 300px;
+  height: 145px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top:15px;
+}
+.title{
+  line-height: 350px;
+}
+a {
+  position: absolute;
+  left: 225px;
+  top: 315px;
+
+  /* margin-top: 20px;
+margin-left: 20px; */
+}
+.link {
+  text-decoration: underline;
+  font-size: 12px;
+}
 </style>
