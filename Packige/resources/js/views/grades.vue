3b6e9a0f5c4dd96098aeb5c4fb92040a928789fb
Paul Mairot <paul.mairot@heig-vd.ch>
2022-06-09 11:09:19 +0200

    Styling grades tab

--- Packige/resources/js/views/grades.vue
+++ Packige/resources/js/views/grades.vue
@@ -21,23 +21,73 @@ let showModal = ref(false);
 </script>
 
 <template>
+  <h1>Notes</h1>
   <GradeModal v-show="showModal" @close="showModal = false" :id="id" />
   <button @click="addGrade()">Ajouter une note</button>
 
-  <div v-for="(moduleData, moduleName) in grades">
-    <h1>{{ moduleName }}</h1>
-    Moyenne de module : {{ moduleData.average }}
+  <div v-for="(moduleData, moduleName) in grades" class="module">
+    <h2>{{ moduleName }}</h2>
     <div v-for="(courseData, courseName) in moduleData">
       <div v-if="courseName != 'average'">
-        <h2>{{ courseName }}</h2>
-        Pondération : {{ courseData.weighting }}
+        <h3>{{ courseName }}</h3>
+        <span class="courseWeighting">{{ courseData.weighting }}</span>
         <br />
         <div v-for="gradeData in courseData.grades" @click="editGrade(gradeData);">
           Note : {{ gradeData.grade }} | Pondération :
           {{ gradeData.coefficient }}
         </div>
-        Moyenne : {{ courseData.average }}
+        <span class="course">Moyenne : {{ courseData.average }}</span>
       </div>
     </div>
+    Moyenne de module : {{ moduleData.average }}
   </div>
 </template>
+
+<style scoped>
+  .module {
+    padding: 25px;
+
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 40px;
+
+    width: 90%;
+
+    border: 3px solid;
+    border-image: linear-gradient(28.82deg, #C8D7F4 -39.19%, #F84E35 3.81%, #77B0C5 52.2%, #0C223F 116.48%) 1;
+    border-radius: 25px;
+
+  }
+
+  .module h2 {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 30px;
+  }
+
+  .module h3 {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 30px;
+
+    width: 80%;
+
+    display: inline-block;
+
+    margin-left: 5px;
+  }
+
+  .module .courseWeighting {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 30px;
+
+    opacity: 0.6;
+  }
+</style>
