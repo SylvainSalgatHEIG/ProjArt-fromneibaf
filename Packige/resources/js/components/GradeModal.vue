35e4a6fc6cf080e246e6feec23a270e4fd8021b1
Paul Mairot <paul.mairot@heig-vd.ch>
2022-06-10 10:27:44 +0200

    Modal grades

--- Packige/resources/js/components/GradeModal.vue
+++ Packige/resources/js/components/GradeModal.vue
@@ -140,14 +140,16 @@ function deleteGrade(data, courseShortname, moduleName) {
   <transition name="modal">
     <div class="modal-mask">
       <div class="modal-wrapper">
-        <div class="modal-container">
+        <div class="modal-block">
+
+          <div class="modal-default-button closeButton" @click="$emit('close')"></div>
+
           <div class="modal-header">
             <slot name="header"> {{ btnText }} une note </slot>
           </div>
           <div class="modal-body">
             <slot name="body">
               <form @submit.prevent="addOrEditGrade()">
-                <label for="grade">Note :</label><br />
                 <input
                   type="number"
                   v-model="grade"
@@ -156,16 +158,16 @@ function deleteGrade(data, courseShortname, moduleName) {
                   required
                   placeholder="Note obtenue"
                 />
-                <label for="coefficient">Coefficient :</label><br />
+                <br />
                 <input
                   type="number"
                   v-model="coefficient"
                   id="coefficient"
                   step="0.01"
                   required
-                  placeholder="Pondération"
+                  placeholder="Pondération %"
                 />
-                <label for="course">Cours :</label><br />
+                <br />
                 <select id="course" v-model="course" :disabled="disabledSelect" placeholder="Branche">
                 <option value="" disabled selected>Branche</option>
                   <option
@@ -176,9 +178,11 @@ function deleteGrade(data, courseShortname, moduleName) {
                   </option>
                 </select>
                 <br />
-                <button class="modal-default-button" v-bind:class="btnText == 'Ajouter' ? 'addButton': 'updateButton'">
+                  
+              </form>
+              <button class="" v-bind:class="btnText == 'Ajouter' ? 'addButton': 'updateButton'">
                   {{ btnText }}
-                </button>
+                  </button>
                 <button
                   class="modal-default-button"
                   v-show="disabledSelect"
@@ -186,26 +190,20 @@ function deleteGrade(data, courseShortname, moduleName) {
                 >
                   del
                 </button>
-              </form>
             </slot>
           </div>
 
-          <div class="modal-footer">
-            <slot name="footer">
-              <div class="modal-default-button closeButton" @click="$emit('close')">
-              </div>
-            </slot>
-          </div>
+              
         </div>
       </div>
     </div>
   </transition>
 </template>
 
-<style>
+<style scoped>
 div.modal-mask {
   position: fixed;
-  z-index: 9998;
+  z-index: 9999;
   bottom: 0;
   left: 0;
   width: 100%;
@@ -213,20 +211,21 @@ div.modal-mask {
   background-color: rgba(0, 0, 0, 0.5);
   display: table;
   transition: opacity 0.3s ease;
+
 }
 
 div.modal-wrapper {
   display: table-cell;
   vertical-align: bottom;
 
-  width: 100%;
+  max-width: 500px;
 }
 
-div.modal-container {
-  width: 100%;
+div.modal-block {
+  max-width: 500px !important;
 
-  margin: 0px;
-  padding: 0px 30px 44px 30px;
+  margin: auto;
+  padding: 0px 0px 10px 0px;
 
   background-color: #fff;
 
@@ -234,7 +233,6 @@ div.modal-container {
 
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
 }
 
 div.modal-header {
@@ -245,7 +243,6 @@ div.modal-header {
 
   border-bottom: none;
 
-  margin-top: 44px;
   padding-left: auto;
 
   text-align: center;
@@ -255,24 +252,82 @@ div.modal-header {
   font-weight: 700;
   font-size: 32px;
   line-height: 36px;
+
+  color: #0C223F;
 }
 
 .modal-body {
-  margin: 20px 0;
+  margin: 0 !important;
 }
 
+.modal-body form {
+  width: 250px;
+  margin: auto;
+}
+
+input {
+  border: none;
+  border-bottom: 1px solid #0C223F;
+  width: 100px;
+  margin-bottom: 40px;
+}
+
+/* Remove arrow input type number */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type=number] {
+  -moz-appearance: textfield;
+}
+
+select {
+  -webkit-tap-highlight-color: transparent;
+        
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+
+
+        background-image: url("data:image/svg+xml,%3Csvg width='24' height='26' viewBox='0 0 24 26' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M6 9.75L12 16.25L18 9.75' stroke='%230C223F' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E%0A");
+        background-size: 25px;
+        background-position: calc(100% + .1rem);
+        background-repeat: no-repeat;
+
+        width: 150px;
+
+        border: none;
+        border-radius: 0%;
+        border-bottom: 1px solid #0C223F;
+
+        color: #0C223F;
+        opacity: 0.6;
+
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 22px;
+
+}
+
+
+
+
 .addButton {
+
+  margin: 50px auto 0 auto;
+
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  padding: 16px 50px;
   gap: 10px;
 
   width: 290px;
   height: 45px;
-
-  margin-top: 88px;
 
   background: #F84E35;
   border-radius: 40px;
@@ -290,13 +345,21 @@ div.modal-header {
 
 .closeButton {
 
-  background-color: #F84E35;
-  height: 10px;
-  width: 10px;
+
+  height: 35px;
+  width: 35px;
+
+  margin: 10px 10px auto auto;
+
+  background-image: url("data:image/svg+xml,%3Csvg width='34' height='36' viewBox='0 0 34 36' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11.3205 25.6075C10.9392 25.6075 10.5579 25.4587 10.2568 25.14C9.67481 24.5237 9.67481 23.5037 10.2568 22.8875L21.6161 10.86C22.1981 10.2437 23.1615 10.2437 23.7435 10.86C24.3255 11.4762 24.3255 12.4962 23.7435 13.1125L12.3842 25.14C12.1032 25.4587 11.7018 25.6075 11.3205 25.6075Z' fill='%230C223F'/%3E%3Cpath d='M22.6798 25.6075C22.2985 25.6075 21.9172 25.4587 21.6161 25.14L10.2568 13.1125C9.67481 12.4962 9.67481 11.4762 10.2568 10.86C10.8388 10.2437 11.8022 10.2437 12.3842 10.86L23.7435 22.8875C24.3255 23.5037 24.3255 24.5237 23.7435 25.14C23.4424 25.4587 23.0611 25.6075 22.6798 25.6075Z' fill='%230C223F'/%3E%3C/svg%3E%0A");
+  background-repeat: no-repeat;
+  
 }
 
-.modal-footer {
+div.modal-footer {
   border-top: none;
+  width: 100%;
+  
 }
 
 
