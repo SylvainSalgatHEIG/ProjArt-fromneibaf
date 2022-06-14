8010f1e4cbcbad374eca50f049d7f12a3e37b189
Paul Mairot <paul.mairot@heig-vd.ch>
2022-06-14 16:28:25 +0200

    Trying disable zoom

--- Packige/resources/views/vue.blade.php
+++ Packige/resources/views/vue.blade.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
 	<title>Fromneibaf</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 	<link rel="manifest" href="manifest.webmanifest" />
